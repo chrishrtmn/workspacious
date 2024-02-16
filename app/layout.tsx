35a9a492e32873dae5d184c308779011cbc37ff3
Chris Hartman <chrishartman@me.com>
2024-02-16 14:54:45 -0500

    Fixing filepaths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import dynamic from "next/dynamic";
 import { Inter } from "next/font/google";
+import "../assets/styles/globals.css";
 import { PHProvider } from "./providers";
-import "./styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 const PostHogPageView = dynamic(() => import("../utilities/PostHogPageView"), {
